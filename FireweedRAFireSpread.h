d6b13d14aa5c1d906617f28784ccf44fb95185ab
Joshua Rady <joshuarady@Steppin-Razor.local>
2024-07-09 18:23:48 -0400

    Update header notes.

--- FireweedRAFireSpread.h
+++ FireweedRAFireSpread.h
@@ -5,9 +5,9 @@ Woodwell Climate Research Center
 Started: 1/29/2024
 Reference: Proj. 11 Exp. 14
 
-	This header file declares a set of functions implementing the Rothermel fire spread model
-(Rothermel 1972) with the modifications of Albini (Albini 1976).  This is part header file is part
-of the Fireweed fire code library.
+	This file is part of the Fireweed fire code library.  It declares a set of functions
+implementing the Rothermel fire spread model (Rothermel 1972) with the modifications of Albini
+(Albini 1976) of the Fireweed fire code library.
 
 ***************************************************************************************************/
 #ifndef FIREWEEDRAFIRESPREAD_H
