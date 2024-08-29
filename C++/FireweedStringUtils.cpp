3c7b2f0640d245d06b190b8bd4a89a84d81751d2
Joshua Rady <joshuarady@Steppin-Razor.local>
2024-08-29 17:57:11 -0400

    Add C++/FireweedStringUtils.h/.cpp as empty shell.

---
+++ C++/FireweedStringUtils.cpp
@@ -0,0 +1,12 @@
+/***************************************************************************************************
+FireweedStringUtils.cpp
+Programmed by: Joshua M. Rady
+Woodwell Climate Research Center
+Started: 8/29/2024
+Reference: Proj. 11 Exp. 20
+
+	This file is part of the Fireweed wildfire code library.  It defines a set of string utilities.
+
+***************************************************************************************************/
+
+#include "FireweedStringUtils.h"
---
+++ C++/FireweedStringUtils.h
@@ -0,0 +1,19 @@
+/***************************************************************************************************
+FireweedStringUtils.h
+Programmed by: Joshua M. Rady
+Woodwell Climate Research Center
+Started: 8/29/2024
+Reference: Proj. 11 Exp. 20
+
+	This file is part of the Fireweed wildfire code library.  This header file declares a set of
+string utilities.
+
+***************************************************************************************************/
+#ifndef FIREWEEDSTRINGUTILS_H
+#define FIREWEEDSTRINGUTILS_H
+
+#include <vector>
+
+
+
+#endif //FIREWEEDSTRINGUTILS_H
