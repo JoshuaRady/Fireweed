50c8b3351912f0b6cdfc05acd6444c5d1468f712
Joshua Rady <joshuarady@Steppin-Razor.local>
2024-09-16 16:53:17 -0400

    Create shell of FireweedMetUtils.r.

---
+++ R/FireweedMetUtils.r
@@ -0,0 +1,21 @@
+#FireweedMetUtils.r
+#Programmed by: Joshua M. Rady
+#Woodwell Climate Research Center
+#2024
+#Reference: Proj. 11 Exp. 20
+#
+#Description:---------------------------------------------------------------------------------------
+#  This is part of the Fireweed wildfire code library.
+#  This file contains utility function for meteorological properties.
+#
+#References:----------------------------------------------------------------------------------------
+#
+#...
+#
+#___________________________________________________________________________________________________
+
+# source("FireweedUnits.r")
+# source("FireweedUtils.r")
+
+#Code:----------------------------------------------------------------------------------------------
+
