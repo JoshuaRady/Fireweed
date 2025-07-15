5c3e6cb362d8c052c906422b6ff08860f8059059
Joshua Rady <joshuarady@Steppin-Razor.local>
2025-07-15 13:57:45 -0400

    Change argX parameters to vecX.

--- C++/FireweedUtils.cpp
+++ C++/FireweedUtils.cpp
@@ -14,78 +14,7 @@ Reference: Proj. 11 Exp. 19
 
 #include "FireweedUtils.h"
 
-/** This utility checks that the parameters (vectors) passed have the same length.  2-4 and 7
- * arguments are accepted.
- *
- * Overloading has been used to reproduce the behavior of the R function, although that function can
- * handle arguments of different types in any order.  We only handle a subset of possible type
- * combinations.  This function is currently used primarily for variable vectors of type double and
- * liveDead, which is currently an integer but could be a boolean.  To reduce the number of
- * possibilities we require liveDead to be last.  It might be possible to do this more compactly
- * with template functions or ariadic arguments.
- *
- * @param arg1, arg2, ... A series of vector argument to compare.
- *
- * @returns Do the vectors have the same number of elements.
- *
- * Could change arguments to const &?
- */
-// bool SameLengths(std::vector<double> arg1, std::vector<double> arg2)
-// {
-// 	return (arg1.size() == arg2.size());
-// }
-
-// template <typename T1, typename T2>
-// bool SameLengthsT(const std::vector<T1>& arg1, const std::vector<T2>& arg2)
-// {
-// 	return (arg1.size() == arg2.size());
-// }
-
-// bool SameLengths(std::vector<double> arg1, std::vector<int> arg2)
-// {
-//  	return (arg1.size() == arg2.size());
-// }
-
-// bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<double> arg3)
-// {
-// 	return (SameLengths(arg1, arg2) && SameLengths(arg1, arg3));
-// }
-
-// bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<int> arg3)
-// {
-// 	return (SameLengths(arg1, arg2) && SameLengths(arg1, arg3));
-// }
-
-// bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<double> arg3,
-//                  std::vector<double> arg4)
-// {
-// 	return (SameLengths(arg1, arg2) && SameLengths(arg1, arg3) && SameLengths(arg1, arg4));
-// }
-// 
-// bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<double> arg3,
-//                  std::vector<int> arg4)
-// {
-// 	return (SameLengths(arg1, arg2) && SameLengths(arg1, arg3) && SameLengths(arg1, arg4));
-// }
-
-//7 doubles:
-// bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<double> arg3,
-//                  std::vector<double> arg4, std::vector<double> arg5, std::vector<double> arg6,
-//                  std::vector<double> arg7)
-// {
-// 	return (SameLengths(arg1, arg2) && SameLengths(arg1, arg3) && SameLengths(arg1, arg4) &&
-// 			SameLengths(arg1, arg5) && SameLengths(arg1, arg6) && SameLengths(arg1, arg7));
-// }
-
-//7 doubles and an int vector:
-// bool SameLengths(std::vector<double> dbl1, std::vector<double> dbl2, std::vector<double> dbl3,
-//                  std::vector<double> dbl4, std::vector<double> dbl5, std::vector<double> dbl6,
-//                  std::vector<double> dbl7, std::vector<int> int8)
-// {
-// 	return (SameLengths(dbl1, dbl2) && SameLengths(dbl1, dbl3) && SameLengths(dbl1, dbl4) &&
-// 			SameLengths(dbl1, dbl5) && SameLengths(dbl1, dbl6) && SameLengths(dbl1, dbl7) &&
-// 			SameLengths(dbl1, int8));
-// }
+//See the header for SameLengths().
 
 //This utility checks that a value falls in a valid range.
 bool InRange(double value, double low, double high)
--- C++/FireweedUtils.h
+++ C++/FireweedUtils.h
@@ -14,52 +14,46 @@ Reference: Proj. 11 Exp. 19
 
 #include <vector>
 
-//bool SameLengths(std::vector<double> arg1, std::vector<double> arg2);
-
+/** The SameLengths() utility checks that the vectors passed have the same length. 2-4 and 7
+ * vecuments are accepted.
+ *
+ * Overloading and templates have been used to handle vector vecuments of different types in any
+ * order reproducing the behavior of the R function.
+ *
+ * @param vec1, vec2, ... A series of vector vecument to compare.
+ *
+ * @returns Do the vectors have the same number of elements.
+ */
 template <typename T1, typename T2>
-bool SameLengths(const std::vector<T1>& arg1, const std::vector<T2>& arg2)
+bool SameLengths(const std::vector<T1>& vec1, const std::vector<T2>& vec2)
 {
-	return (arg1.size() == arg2.size());
+	return (vec1.size() == vec2.size());
 }
 
-//bool SameLengths(std::vector<double> arg1, std::vector<int> arg2);
-//bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<double> arg3);
 template <typename T1, typename T2, typename T3>
-bool SameLengths(const std::vector<T1>& arg1, const std::vector<T2>& arg2, const std::vector<T3>& arg3)
+bool SameLengths(const std::vector<T1>& vec1, const std::vector<T2>& vec2, const std::vector<T3>& vec3)
 {
-	return (SameLengths(arg1, arg2) && SameLengths(arg1, arg3));
+	return (SameLengths(vec1, vec2) && SameLengths(vec1, vec3));
 }
 
-//bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<int> arg3);
-
-// bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<double> arg3,
-//                  std::vector<double> arg4);
-// bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<double> arg3,
-//                  std::vector<int> arg4);
 template <typename T1, typename T2, typename T3, typename T4>
-bool SameLengths(const std::vector<T1>& arg1, const std::vector<T2>& arg2,
-                 const std::vector<T3>& arg3, const std::vector<T4>& arg4)
+bool SameLengths(const std::vector<T1>& vec1, const std::vector<T2>& vec2,
+                 const std::vector<T3>& vec3, const std::vector<T4>& vec4)
 {
-	return (SameLengths(arg1, arg2) && SameLengths(arg1, arg3) && SameLengths(arg1, arg4));
+	return (SameLengths(vec1, vec2) && SameLengths(vec1, vec3) && SameLengths(vec1, vec4));
 }
 
-// bool SameLengths(std::vector<double> arg1, std::vector<double> arg2, std::vector<double> arg3,
-//                  std::vector<double> arg4, std::vector<double> arg5, std::vector<double> arg6,
-//                  std::vector<double> arg7);
 template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
-bool SameLengths(const std::vector<T1>& arg1, const std::vector<T2>& arg2,
-                 const std::vector<T3>& arg3, const std::vector<T4>& arg4,
-                 const std::vector<T5>& arg5, const std::vector<T6>& arg6,
-                 const std::vector<T7>& arg7)
+bool SameLengths(const std::vector<T1>& vec1, const std::vector<T2>& vec2,
+                 const std::vector<T3>& vec3, const std::vector<T4>& vec4,
+                 const std::vector<T5>& vec5, const std::vector<T6>& vec6,
+                 const std::vector<T7>& vec7)
 {
-	return (SameLengths(arg1, arg2) && SameLengths(arg1, arg3) && SameLengths(arg1, arg4) &&
-			SameLengths(arg1, arg5) && SameLengths(arg1, arg6) && SameLengths(arg1, arg7));
+	return (SameLengths(vec1, vec2) && SameLengths(vec1, vec3) && SameLengths(vec1, vec4) &&
+			SameLengths(vec1, vec5) && SameLengths(vec1, vec6) && SameLengths(vec1, vec7));
 }
 
-// bool SameLengths(std::vector<double> dbl1, std::vector<double> dbl2, std::vector<double> dbl3,
-//                  std::vector<double> dbl4, std::vector<double> dbl5, std::vector<double> dbl6,
-//                  std::vector<double> dbl7, std::vector<int> int8);
-template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
+/template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
           typename T8>
 bool SameLengths(const std::vector<T1>& vec1, const std::vector<T2>& vec2,
                  const std::vector<T3>& vec3, const std::vector<T4>& vec4,
