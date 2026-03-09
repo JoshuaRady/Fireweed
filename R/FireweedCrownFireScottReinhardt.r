8b2aa6e7b12e1f5298b546edd8df24d6129aec99
Joshua Rady <joshuarady@Steppin-Razor.local>
2026-03-09 12:22:11 -0400

    Clean up code and comments.

--- R/FireweedCrownFireScottReinhardt.r
+++ R/FireweedCrownFireScottReinhardt.r
@@ -38,8 +38,7 @@ source("FireweedRAFireSpread.r")
 #the spread calculations in United States customary units or metric units.  We choose to do
 #everything in metric units so we replace the original constant with a conversion from km/hr to m/min:
 #kmPerHrToMPerMin = 1000 / 60#1000 m/km / 60 min/hr
-kmPerHrToMPerMin = 54.683
-tempUnits = "Metric"#"US"
+WindConversionFactor = 54.683
 
 #Code:----------------------------------------------------------------------------------------------
 
@@ -88,14 +87,15 @@ CriticalCrowningIntensityVanWagner <- function(CBH, FMC)
 
 #' Return the critical surface fire rate of spread needed to initiate crowning using Van Wagner 1977.
 #'
-#' @param I_initiation (kW/m, I_0 in Van Wagner notation, I'_initiation in Scott & Reinhardt notation).
+#' @param IPrime_initiation (kW/m, I_0 in Van Wagner notation, I'_initiation in Scott & Reinhardt
+#'                           notation).
 #' @param HPA Surface fire heat per area (kw/m^2).
 #'
 #' @returns The critical surface fire rate of spread for crowning (m/min).
-CriticalCrowningROSVanWagner <- function(I_initiation, HPA)
+CriticalCrowningROSVanWagner <- function(IPrime_initiation, HPA)
 {
   #Scott & Reinhardt 2001 equation 12, pg. 13:
-  R_initiation = (60 * I_initiation) / HPA
+  R_initiation = (60 * IPrime_initiation) / HPA
   return(R_initiation)
 }
 
@@ -114,11 +114,11 @@ CriticalActiveROSVanWagner <- function(CBD)
   return(Rprime_active)
 }
 
-#' Calculate the torching index, the open wind speed?? at which torching starts
+#' Calculate the torching index, the open wind speed at which torching starts,
 #'
 #' @param spreadCalcs The spread calculations (list) output from the SpreadRateRothermelAlbini_*()
 #' family of functions.
-#' @param WRF Wind reduction factor.  Ratio used to convert from mid-flame to open wind speed.
+#' @param WRF Wind reduction factor.  Ratio to convert from open (6.1 m) to mid-flame wind speed.
 #' @param CBH Crown base height (m, z in original Van Wagner notation).
 #' @param FMC Foliar moisture content of (conifer) canopy (%, water weight/dry fuel weight x 100)
 #'
@@ -128,53 +128,50 @@ TorchingIndex <- function(spreadCalcs, WRF, CBH, FMC)
   #Scott & Reinhardt 2001 equation 18, pg. 18:
   #TI = (1 / (54.683 * WRF)) * ((((60 * Iprime_initiation * rho_b * epsilon * Q_ig)/(HPA * xi * IR))
   #     - phi_s - 1) / (C * beta/beta_op)))^1/B
-  #We break this equation up into the follow parts for readability:
+  #We break this equation up into the follow parts and compute them in steps:
   #TI = factor_L * (numerator_R / denominator_R)^1/B
   
   #We need the three wind factors:
-  C = WindFactorC(spreadCalcs$cSAV, tempUnits)
-  B = WindFactorB(spreadCalcs$cSAV, tempUnits)
-  E = WindFactorE(spreadCalcs$cSAV, tempUnits)
-  #WRF = wind reduction factor, the ratio of midflame wind speed to open wind speed...
+  C = WindFactorC(spreadCalcs$cSAV, "Metric")
+  B = WindFactorB(spreadCalcs$cSAV, "Metric")
+  E = WindFactorE(spreadCalcs$cSAV, "Metric")
   
   #The leftmost factor:
-  #factor_L = 1 / (54.683 * WRF)
-  factor_L = 1 / (kmPerHrToMPerMin * WRF)
+  factor_L = 1 / (WindConversionFactor * WRF)
   
   #The numerator on the right is Scott & Reinhardt 2001 equation 16 pg. 18:
-  #phiPrime_w_intiation = ((60 * IPrime_initiation * HeatSink)/(HPA * xi * I_R)) - phi_s - 1
-  #[Bulk density]?????
-  
-  #The numerator of equation 16 contains the full heat sink (rho_b * epsilon * Q_ig) where
-  #rho_b = Bulk density
-  #...
-  #and:
+  #phiPrime_w_intiation = ((60 * IPrime_initiation * rho_b * epsilon * Q_ig)/(HPA * xi * I_R))
+  #                       - phi_s - 1
+  #Where;
+  #rho_b = bulk density
+  #epsilon = the effective heating number
+  #Q_ig = heat of preignition
   #HPA = heat per area
-  #xi = Effective heating number
+  #xi = the propagating flux ratio
   #I_R = Reaction intensity
   #phi_s = slope factor
   
-  IPrime_initiation = CriticalCrowningIntensityVanWagner(CBH, FMC)
-  HPA = HeatPerUnitArea(spreadCalcs$I_R, ResidenceTime(spreadCalcs$cSAV, tempUnits))
+  #The numerator of equation 16 contains the full heat sink (rho_b * epsilon * Q_ig).
+  #We substitute for this since we do not calculate epsilon directly for heterogeneous fuels:
+  #phiPrime_w_intiation = ((60 * IPrime_initiation * HeatSink)/(HPA * xi * I_R)) - phi_s - 1
   
+  IPrime_initiation = CriticalCrowningIntensityVanWagner(CBH, FMC)
+  HPA = HeatPerUnitArea(spreadCalcs$I_R, ResidenceTime(spreadCalcs$cSAV, "Metric"))
   phiPrime_w_initiation = ((60 * IPrime_initiation * spreadCalcs$HeatSink) /
                              (HPA * spreadCalcs$xi * spreadCalcs$I_R)) - spreadCalcs$phi_s - 1
   
-  #The denominator on the right consists of most of the terms from the wind factor calculation.  It
-  #has been rearranged to remove U.
+  #The denominator on the right consists of most of the Rothermel & Albini spread model wind factor
+  #calculation.  It has been rearranged to remove U:
   #C * (beta / beta_opt)^-E
-  #beta = mean packing ratio
-  #beta_op = optimum packing ratio
-  #beta/beta_op = relative packing ratio
   denominator_R = C * (spreadCalcs$MeanPackingRatio / spreadCalcs$OptimumPR)^-E
   
   #The complete calculation:
-  TI = factor_L * (phiPrime_w_initiation / denominator_R)^(1/B)#1/B
+  TI = factor_L * (phiPrime_w_initiation / denominator_R)^(1/B)
   
   return(TI)
 }
 
-#' Calculate the crowning index, the open wind speed?? at which crowning starts.
+#' Calculate the crowning index, the open wind speed at which active crowning occurs.
 #'
 #' @param spreadCalcs The spread calculations (list) output from the SpreadRateRothermelAlbini_*()
 #' family of functions.
@@ -183,49 +180,44 @@ TorchingIndex <- function(spreadCalcs, WRF, CBH, FMC)
 #' @returns CI, the crowning index (open wind speed at 6.1 m, km/hr).
 CrowningIndex <- function(spreadCalcs, CBD)
 {
-  #This is Scott & Reinhardt 2001 equation 19, pg. 19:
-  #...
-  #We break this equation up into the follow parts for readability:
+  #Scott & Reinhardt 2001 equation 19, pg. 19 gives the midflame wind speed for active crowning.
+  #UPrime_active = (1/54.683) * ((((3.0 / CBD) * rho_b * epsilon * Q_ig) / (3.34 * I_R * xi)
+  #                               - spreadCalcs$phi_s - 1) / (C * (beta/beta_op)^-E))^1/B
+  #To get the open wind speed for active crowning (O'_active = CI) using the assumptions of
+  #Rothermel 1991 we can divide by a 40% wind reduction factor.
+  #We break the resulting equation up into the follow parts and compute them in steps:
   #CI = factor_L * (numerator_R / denominator_R)^1/B
-  #Note: Scott & Reinhardt 2001 equation 20 is a simplifed version that assumes parameters of fuel
-  #model 10.  We use equation 19 for flexibility.  It allows the user to break this assumption.
   
   #We need the three wind factors:
-  #tempUnits = "US"
-  C = WindFactorC(spreadCalcs$cSAV, tempUnits)
-  B = WindFactorB(spreadCalcs$cSAV, tempUnits)
-  E = WindFactorE(spreadCalcs$cSAV, tempUnits)
+  C = WindFactorC(spreadCalcs$cSAV, "Metric")
+  B = WindFactorB(spreadCalcs$cSAV, "Metric")
+  E = WindFactorE(spreadCalcs$cSAV, "Metric")
   
   #The leftmost factor:
-  #factor_L = 1 / 54.683
-  #factor_L = 1 / kmPerHrToMPerMin
-  factor_L = 1 / 54.683 / 0.4
+  factor_L = 1 / WindConversionFactor / 0.4
   
   #The numerator on the right is:
-  #(((3.0 / CBD) * rho_b * epsilon * Q_ig) / 3.34 * I_R * xi) - phi_s - 1
+  #(((3.0 / CBD) * rho_b * epsilon * Q_ig) / (3.34 * I_R * xi)) - phi_s - 1
   #Which is the same as:
   #(((3.0 / CBD) * HeatSink) / 3.34 * I_R * xi) - phi_s - 1
-  # numerator_R = (((3.0 / CBD) * spreadCalcs$HeatSink) /
-  #                  3.34 * spreadCalcs$I_R * spreadCalcs$xi) - spreadCalcs$phi_s - 1
   numerator_R = (((3.0 / CBD) * spreadCalcs$HeatSink) /
                    (3.34 * spreadCalcs$I_R * spreadCalcs$xi)) - spreadCalcs$phi_s - 1
   
-  #The denominator on the right consists of most of the terms from the wind factor calculation.  It
-  #has been rearranged to remove U.  (It is the same as in the torching index.)
+  #The denominator on the right consists of most of the Rothermel & Albini spread model wind factor
+  #calculation.  It has been rearranged to remove U  (It is the same as in the torching index.):
   #C * (beta / beta_opt)^-E
-  #beta = mean packing ratio
-  #beta_op = optimum packing ratio
-  #beta/beta_op = relative packing ratio
   denominator_R = C * (spreadCalcs$MeanPackingRatio / spreadCalcs$OptimumPR)^-E
   
   #The complete calculation:
-  CI = factor_L * (numerator_R / denominator_R)^(1/B)#1/B
-  #CI = (1/54.683 / 0.4) * (numerator_R / denominator_R)^(1/B)#1/B
-  #CI = 0.0457 * (numerator_R / denominator_R)^(1/B)#1/B
+  CI = factor_L * (numerator_R / denominator_R)^(1/B)
   
-  #Equation 20:
-  # CI2 = 0.0457 * ((((3.0 / 3.34 / spreadCalcs$xi * spreadCalcs$HeatSink) /
-  #                     (spreadCalcs$I_R * CBD))- spreadCalcs$phi_s - 1) / 0.001612)^0.7
+  #Note: Scott & Reinhardt 2001 uses equation 20, which is a simplified version of the above that
+  #assumes parameters of fuel #model 10:
+  #CI = 0.0457 * ((((3.0 / 3.34 / spreadCalcs$xi * spreadCalcs$HeatSink) /
+  #                   (spreadCalcs$I_R * CBD))- spreadCalcs$phi_s - 1) / 0.001612)^0.7
+  #We opt not to use the simplification to keep the code more parallel to the torching index
+  #calculation and because rounding and small differences in conversion factors lead to slight
+  #differences in the values we get with the explicit calculation.
   
   return(CI)
 }
@@ -235,10 +227,11 @@ CrowningIndex <- function(spreadCalcs, CBD)
 #' The representation in Scott & Reinhardt 2001 does not purport to actually predict the crown
 #' fraction burned for its own sake.  Instead it is used as a transition function between surface
 #' and active crown fire spread rate calculations.  However, this has not stopped people from using
-#' the output as an approximation of actual CFB and we do so well.
+#' the output as an approximation of actual CFB..
 #'
-#' @param fm10 Fuel model 10 with fuel loadings. ... 
-#' M_f_ij must be included in the fuel model.
+#' @param fm10 The fuel model representing the surface fuelbed.  M_f_ij must be included in the fuel
+#' model.  The fuel model should be converted to the properties of fuel model 10 if it is not. 
+#' 
 #' Expect FM 10 and change it if not?  We shoud have an option to not change it.  No, we still need
 #' the data from fuel model 10 so it needs to be passed in.
 #' 
@@ -246,7 +239,7 @@ CrowningIndex <- function(spreadCalcs, CBD)
 #' #I guess O = U when WRF = 1????
 #' @param U Wind speed at midflame height (m/min).
 #' @param O Open wind speed at 6.1 m (km/hr)
-#' @param WRF Wind reduction factor.  Ratio used to convert from mid-flame to open wind speed.
+#' @param WRF Wind reduction factor.  Ratio to convert from open (6.1 m) to mid-flame wind speed.
 #' 
 #' @param slopeSteepness Slope steepness, maximum (unitless fraction: vertical rise / horizontal
 #'                       distance).
@@ -259,9 +252,7 @@ CrowningIndex <- function(spreadCalcs, CBD)
 CrownFractionBurned <- function(fm10, #U,
                                 O, WRF,
                                 slopeSteepness,
-                                #spreadCalcs,
                                 CBH, FMC, CBD)
-                                #useWindLimit = FALSE)#This probably has an assumed value?????
 {
   #Check for M_f_ij in the incoming fuel model.
   
@@ -272,26 +263,24 @@ CrownFractionBurned <- function(fm10, #U,
   }
   #Add check for unexpectedly high wind speeds?
   
-  #If O is passed in we need to calculate U...
-  #U = O * WRF
-  U = O * WRF * kmPerHrToMPerMin
-  #U = O * 0.4#See pg. 11...
-  #kmPerHrToMPerMin here is probalby wrong, though it doesn't matter since U is irrelevant here!
+  #If O is passed in we need to calculate U:
+  U = O * WRF * WindConversionFactor
   
+  #The paper never mentions the wind limit and we assume it is not used:
   spreadCalcs = SpreadRateRothermelAlbini_HetFM(fm10, U, slopeSteepness, components = TRUE)
   
   TI = TorchingIndex(spreadCalcs, WRF, CBH, FMC)
   CI = CrowningIndex(spreadCalcs, CBD)
   
-  if (O < TI)#Surface fire
+  if (O < TI)#Surface fire:
   {
     CFB = 0.0
   }
-  else if (O >= CI)#Fully active crown fire
+  else if (O >= CI)#Fully active crown fire:
   {
     CFB = 1.0
   }
-  else#Torching to full crown involvement:
+  else#Passive crown fire, torching to full crown involvement:
   {
     #Calculate the transition as a linear function of wind speed:
     CFB = (1 / (CI - TI)) * (O - TI)
